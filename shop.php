da6cb6406bc2d4be580bf504160032e65e413620
ahwz <andreas.zincke@gmx.at>
2025-01-15 13:32:55 +0100

    start shop page

--- shop.php
+++ shop.php
@@ -5,14 +5,31 @@ if(!$_SESSION || !isset($_SESSION["loggedIn"])) {
 }
 
 ?>
-<pre>
-    <?php print_r($_SESSION);?>
-</pre>
+<!DOCTYPE html>
+<html lang="en">
 
-<form action="" method="post">
-    <button type="submit" name="logoutbtn">Logout</button>
-</form>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Shop Gruppe 2</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style/style.css">
+</head>
 
+<body>
+    <div class="box">
+ <form action="" method="post">
+            <button type="submit" name="logoutbtn">Logout</button>
+        </form>
+
+
+    </div>    
+</body>    
+<!-- <pre>
+    <?php #print_r($_SESSION);?>
+</pre>    
+ -->
 <?php
 if(isset($_POST{"logoutbtn"})) {
     session_unset();
